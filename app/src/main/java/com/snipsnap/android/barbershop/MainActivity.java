5d48c79c8a9afa62b2462ed5f72b0e47ca6be035
Fernando Herrera <fherrera2018@gmail.com>
2020-03-02 20:17:51 -0800

    Integrated more fragments to application. Have created FragmentUtil class to streamline using fragments. Will need to test if this iteration is good

--- app/src/main/java/com/snipsnap/android/barbershop/MainActivity.java
+++ app/src/main/java/com/snipsnap/android/barbershop/MainActivity.java
@@ -2,33 +2,20 @@ package com.snipsnap.android.barbershop;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_signIn;
+    private FragmentUtil fragUtil = new FragmentUtil();
+    private FragmentManager fragMan = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_signIn = findViewById(R.id.go_to_login_page);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        FragmentTransaction frag_transaction = getSupportFragmentManager()
-                .beginTransaction();
-        final Intent intent_signin = new Intent(this, SignInFragment.class);
-        btn_signIn.setOnClickListener(s -> {
-            Fragment frag_signin = new SignInFragment();
-            frag_transaction.add(R.id.main, frag_signin)
-                    .addToBackStack(null)
-                    .commit();
-        });
+        Fragment frag_signInButton = new SignInButtonFragment();
+        fragUtil.addFragment(frag_signInButton, fragMan, R.id.main);
     }
 }
--- app/src/main/java/com/snipsnap/android/barbershop/SignInPageFragment.java
+++ app/src/main/java/com/snipsnap/android/barbershop/SignInPageFragment.java
@@ -9,14 +9,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SignInFragment extends Fragment {
-
+public class SignInPageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in_page,
+                container, false);
         return view;
     }
 }
