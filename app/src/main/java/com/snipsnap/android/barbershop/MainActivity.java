fdb3347e5d85939f9eee2d22be9d968a28c4388f
Fernando <fherrera2018@gmail.com>
2020-04-20 12:11:54 -0700

    Deleted some well meaning but useless fragment files. New base to build apollo-dependencies. I have alredy added some apollo dep stuff in build.gradle and app/build.gradle. Still need to add my schema, queries, and a few more apollo dep stuff.

--- app/src/main/java/com/snipsnap/android/barbershop/MainActivity.java
+++ app/src/main/java/com/snipsnap/android/barbershop/MainActivity.java
@@ -1,21 +1,15 @@
 package com.snipsnap.android.barbershop;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private FragmentUtil fragUtil = new FragmentUtil();
-    private FragmentManager fragMan = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment frag_signInButton = new SignInButtonFragment();
-        fragUtil.addFragment(frag_signInButton, fragMan, R.id.main);
     }
 }
