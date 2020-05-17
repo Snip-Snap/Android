9a62e5320e8101a7ecb4fdd858aefebd997fe92f
Fernando <fherrera2018@gmail.com>
2020-05-17 02:44:50 -0700

    Have added a bottomnavigationview
    BNV does not show up in the main fragment or the login fragment. There are
    two navigations, calendar, and reports. Flesh out reports!

--- app/src/main/java/com/snipsnap/android/barbershop/MainActivity.java
+++ app/src/main/java/com/snipsnap/android/barbershop/MainActivity.java
@@ -1,8 +1,17 @@
 package com.snipsnap.android.barbershop;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +21,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            switch (destination.getId()) {
+                case R.id.mainFragment:
+                case R.id.loginFragment:
+                    bottomNav.setVisibility(View.INVISIBLE);
+                    break;
+                default:
+                    runOnUiThread(() -> bottomNav.setVisibility(View.VISIBLE));
+            }
+        });
     }
 }
